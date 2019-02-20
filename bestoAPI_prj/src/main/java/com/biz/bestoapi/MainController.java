2fdae4ccf7dfdf83032be746373f5812de1c6a0c
kosmo-20 <kosmo-20@DESKTOP-Q228BGD>
2019-02-21 00:57:52 +0900

    1234

--- bestoAPI_prj/src/main/java/com/biz/bestoapi/MainController.java
+++ bestoAPI_prj/src/main/java/com/biz/bestoapi/MainController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
@@ -20,7 +21,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-@Controller
+@RestController
 public class MainController {
 
 	@Autowired
@@ -29,46 +30,23 @@ public class MainController {
 	@Autowired
 	BestoService bestoService;
 
+	@RequestMapping(value="/rankinfo.do", method = RequestMethod.GET)
+	public ArrayList<RankVO> rankinfo() {return bestoService.ranklist();}
+	
 	@RequestMapping(value="/alist.do", method = RequestMethod.GET)
-	public void rankList(HttpServletRequest request,HttpServletResponse response) {
-		try {
-			response.setCharacterEncoding("UTF-8");
-			request.setCharacterEncoding("UTF-8");
-			Gson gson = new Gson();
-			ArrayList<MatchVO> list = bestoService.matchList();
-			String res=gson.toJson(list);
-			System.out.println(res);
-
-			response.setContentType("application/json; encoding=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println(res);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	public ArrayList<MatchVO> rankList() {return bestoService.matchList();}
 
 
 	@RequestMapping(value="/vs.do", method = RequestMethod.GET)
-	public void recordcrawling(
-			HttpServletRequest request,HttpServletResponse response,
+	public VsVO recordcrawling(
 			@RequestParam(value="team1") String team1,@RequestParam(value="team2") String team2) {
 
-		Gson gson = new Gson();
-		ArrayList<String> vslist = new ArrayList<String>();
-		System.out.println(team1);
-		System.out.println(team2);
-
 		String url = "http://www.betman.co.kr/sportsMatchRecord.so?method=inquireMatchRecord&item=SC&league=52&id=1&seq=&teamId1="+ team1 +"&teamId2="+ team2 +"&isToto=&viewType=recent";
-
+		ArrayList<String> vslist = new ArrayList<String>();
 		try {
-			response.setCharacterEncoding("UTF-8");
-			request.setCharacterEncoding("UTF-8");
-			Document doc = Jsoup.connect(url).get();
+			Document doc=Jsoup.connect(url).get();
 			Elements element = doc.select("#resultTeams > div > div > div > div.dataH02WarpSet > div > table > tbody > tr");
-
-			for(Element el : element.select("td")){
-				vslist.add(el.text());
-			}
+			for(Element el : element.select("td")){vslist.add(el.text());}
 
 			for(int i=0;i<vslist.size();i++) {
 				if(i == 0) vsvo.setHomeTeam(vslist.get(i));
@@ -85,53 +63,11 @@ public class MainController {
 				else if(i == 19) vsvo.setHomeGoalavg(vslist.get(i));
 				else if(i == 21) vsvo.setAwayGoalavg(vslist.get(i));
 			}
-
-			String json = gson.toJson(vsvo);
-
-			response.setContentType("application/json; encoding=UTF-8");
-			PrintWriter out = response.getWriter();
-
-			out.println(json);
-
+			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+			return vsvo;
 	}
-
-	@RequestMapping(value="/matchplan.do", method = RequestMethod.GET)
-	public void matchplan(HttpServletRequest request,HttpServletResponse response) {
-		try {
-			response.setCharacterEncoding("UTF-8");
-			request.setCharacterEncoding("UTF-8");
-			Gson gson = new Gson();
-			ArrayList<RankVO> list = bestoService.ranklist();
-			String res=gson.toJson(list);
-			System.out.println(res);
-			PrintWriter out = response.getWriter();
-			out.println(res);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@RequestMapping(value="/rankinfo.do", method = RequestMethod.GET)
-	public void rankinfo(HttpServletRequest request,HttpServletResponse response) {
-		try {
-			response.setCharacterEncoding("UTF-8");
-			request.setCharacterEncoding("UTF-8");
-			Gson gson = new Gson();
-			ArrayList<RankVO> list = bestoService.ranklist();
-			String res=gson.toJson(list);
-			System.out.println(res);
-			response.setContentType("application/json; encoding=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println(res);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-
 }
